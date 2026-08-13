292642d99860d9229f1cf3db2477df070cf9e97b
Saran <msaran449@yahoo.com>
2026-08-13 10:20:22 +0530

    Pipeline execution fix

--- playwright.config.js
+++ playwright.config.js
@@ -1,61 +1,77 @@
-const { defineConfig, devices } = require('@playwright/test');
+import { defineConfig, devices } from '@playwright/test';
 
-module.exports = defineConfig({
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+ 
     testDir: './tests',
 
     timeout: 60000,
 
     expect: {
+
         timeout: 10000
+
     },
 
     fullyParallel: false,
 
-    //workers: process.env.CI ? 1 : undefined,
-
     retries: 1,
 
     reporter: [
+
         ['list'],
+
         ['html']
+
     ],
 
+    use: {
+
+        headless: true,
+
+        viewport: {
+
+            width: 1600,
+
+            height: 900
+
+        },
+
+        ignoreHTTPSErrors: true,
+
+        screenshot: "only-on-failure",
+
+        trace: "retain-on-failure",
+
+        video: "retain-on-failure",
+
+        baseURL: "https://petstore.swagger.io/v2"
+
+    },
+
     projects: [
+
         {
+
             name: 'chromium',
 
             use: {
-                ...devices['Desktop Chrome'],
 
-                headless: !!process.env.CI,
+                ...devices['Desktop Chrome']
 
-                viewport: {
-                    width: 1600,
-                    height: 900
-                },
-
-                launchOptions: {
-                    args: [
-                        '--no-sandbox',
-                        '--disable-setuid-sandbox',
-                        '--disable-dev-shm-usage',
-                        '--disable-gpu',
-                        '--disable-extensions'
-
-                        // ...(process.env.CI
-                        //     ? []
-                        //     : ['--start-maximized'])
-                    ]
-                },
-
-                ignoreHTTPSErrors: true,
-
-                screenshot: 'only-on-failure',
-
-                trace: 'on',
-
-                video: 'retain-on-failure'
             }
+
         }
+
     ]
 });
