97417e2fdfd3668ab9f72ce66ff5d32973802259
Saran <msaran449@yahoo.com>
2026-08-13 09:39:53 +0530

    Pipeline execution fix

--- playwright.config.js
+++ playwright.config.js
@@ -1,71 +1,61 @@
-// @ts-check
 const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
- 
     testDir: './tests',
 
     timeout: 60000,
 
     expect: {
-
         timeout: 10000
-
     },
 
     fullyParallel: false,
 
+    //workers: process.env.CI ? 1 : undefined,
+
     retries: 1,
 
     reporter: [
-
         ['list'],
-
         ['html']
-
     ],
 
-    use: {
-
-        headless: false,
-
-        launchOptions: {
-            args: [
-                '--start-maximized',
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-                '--disable-dev-shm-usage',
-                '--disable-gpu',
-                '--disable-extensions'
-            ]
-        },
-
-        viewport: null,
-
-        ignoreHTTPSErrors: true,
-
-        screenshot: "only-on-failure",
-
-        trace: "on",
-
-        video: "retain-on-failure"
-
-    },
-
     projects: [
-
         {
-
             name: 'chromium',
 
             use: {
+                ...devices['Desktop Chrome'],
 
-                ...devices['Desktop Chrome']
+                headless: !!process.env.CI,
 
+                viewport: {
+                    width: 1600,
+                    height: 900
+                },
+
+                launchOptions: {
+                    args: [
+                        '--no-sandbox',
+                        '--disable-setuid-sandbox',
+                        '--disable-dev-shm-usage',
+                        '--disable-gpu',
+                        '--disable-extensions'
+
+                        // ...(process.env.CI
+                        //     ? []
+                        //     : ['--start-maximized'])
+                    ]
+                },
+
+                ignoreHTTPSErrors: true,
+
+                screenshot: 'only-on-failure',
+
+                trace: 'on',
+
+                video: 'retain-on-failure'
             }
-
         }
-
     ]
 });
-
